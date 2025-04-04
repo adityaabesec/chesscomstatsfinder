14f2bef66a6633fc61165fcf3da618c0ab236e87
AdityaMishra1706 <adityamishrakushinagar@gmail.com>
2025-04-05 04:38:47 +0530

    dynamic port

--- server.js
+++ server.js
@@ -158,4 +158,6 @@ const convertUtcToIst = (utcTimeStr) => {
 };
 
 // Start the server
-app.listen(3000, () => console.log("🚀 Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
